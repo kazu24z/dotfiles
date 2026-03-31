a9b1cc2bb70a9ef432b57a096528fa92c65ad08c
kazu24z <progra.taro2021@gmail.com>
2026-03-31 10:50:56 +0900

    feat(config): cmuxカスタムコマンド設定を追加
    ~/.config/cmux/cmux.json を dotfiles で管理するよう設定。
    install.sh にシンボリックリンク作成を追記。

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ link_or_skip() {
     fi
 }
 
+link_or_skip "$DOTFILES_DIR/config/cmux" ~/.config/cmux
 link_or_skip "$DOTFILES_DIR/config/nvim" ~/.config/nvim
 link_or_skip "$DOTFILES_DIR/config/zellij" ~/.config/zellij
 link_or_skip "$DOTFILES_DIR/config/ghostty" ~/.config/ghostty
